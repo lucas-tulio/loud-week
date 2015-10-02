bee8b4dc6232dcd26409497e1c424414e080edb4
Lucas <lucasdnd@gmail.com>
2015-10-02 20:19:53 -0300

    Added a bunch of random cards for testing

--- core/src/com/lucasdnd/loudweek/gameplay/CardDatabase.java
+++ core/src/com/lucasdnd/loudweek/gameplay/CardDatabase.java
@@ -25,12 +25,22 @@ public class CardDatabase {
 	
 	private CardDatabase() {
 		cards = new ArrayList<CardModel>();
-		cards.add(new CardModel(0, "Abeelha", 2, 1, 5, 3, "", Resources.get().abeelha1));
-		cards.add(new CardModel(1, "Dragonsai", 5, 2, 3, 3, "", Resources.get().abeelha1));
-		cards.add(new CardModel(1, "Golem de Túmulos", 3, 3, 2, 5, "", Resources.get().abeelha1));
-		cards.add(new CardModel(1, "Soldado Ent", 3, 4, 3, 3, "", Resources.get().abeelha1));
-		cards.add(new CardModel(1, "Podridão Sórdida", 4, 3, 2, 4, "", Resources.get().abeelha2));
-	}
+		cards.add(new CardModel(0, "Abeelha",           2, 1, 5, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(1, "Dragonsai",         5, 2, 3, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(2, "Golem de Túmulos",  3, 3, 2, 5, "", Resources.get().abeelha1));
+		cards.add(new CardModel(3, "Soldado Ent",       3, 4, 3, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(4, "Podridão Sórdida",  4, 3, 2, 4, "", Resources.get().abeelha2));
+		
+		cards.add(new CardModel(5, "Abeelha",           6, 1, 5, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(6, "Dragonsai",         5, 2, 3, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(7, "Golem de Túmulos",  2, 3, 7, 5, "", Resources.get().abeelha1));
+		cards.add(new CardModel(8, "Soldado Ent",       1, 5, 5, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(9, "Podridão Sórdida",  4, 3, 5, 4, "", Resources.get().abeelha2));
+		cards.add(new CardModel(10, "Abeelha",          2, 1, 5, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(11, "Dragonsai",        5, 4, 3, 3, "", Resources.get().abeelha1));
+		cards.add(new CardModel(12, "Golem de Túmulos", 3, 3, 5, 5, "", Resources.get().abeelha1));
+		cards.add(new CardModel(13, "Soldado Ent",      3, 2, 3, 4, "", Resources.get().abeelha1));
+		cards.add(new CardModel(14, "Podridão Sórdida", 2, 6, 2, 3, "", Resources.get().abeelha2));}
 	
 	public CardModel getRandomCardModel() {
 		return cards.get(new Random().nextInt(cards.size()));
